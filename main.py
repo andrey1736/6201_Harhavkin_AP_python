2de82a57a41d9d5115ec3a46ba4083b83916efc5
andrey1736 <harhavkin.a@mail.ru>
2025-02-27 09:42:24 +0400

    изменили способ вычисления путем векторизования, улучшили парсинг

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from matplotlib import pyplot as plt
 tree = ET.parse('config.xml')
 root = tree.getroot()
 
-# Читаем данные из файла в переменные
-# Приводим их к типу числа с плавающей точкой из текста
-
 for elem in root.findall('xmin'):
     xmin = float(elem.text)
 for elem in root.findall('xmax'):
@@ -21,20 +18,12 @@ for elem in root.findall('b'):
 for elem in root.findall('c'):
     c = float(elem.text) 
 
-# Открываем файл на запись вычисленных значений
-# В цикле одновременно расчитываем и записываем результаты
-
 file = open('results.txt', 'w')
-x = xmin
-while x <= xmax:
-    y = (((np.sin(a*x)*np.cos(b*x)))/(np.sin(x)+np.cos(x)))+c
-    file.write(str(y) + '\n')
-    x += step
+masX = np.arange(xmin, xmax, step)
+masY = (((np.sin(a*masX)*np.cos(b*masX)))/(np.sin(masX)+np.cos(masX)))+c
+for y in masY:
+    file.write(str(y)+ "\n")
 file.close()
 
-# Создаём линейное пространство с заданными параметрами
-# Рисуем график и показываем его
-
-x = np.linspace(int(xmin), int(xmax), int((xmax - xmin) / step + 1))
-plt.plot(x, (((np.sin(a*x)*np.cos(b*x)))/(np.sin(x)+np.cos(x)))+c)
+plt.plot(masX,masY)
 plt.show()
